f6b8b478f770bad97f02d8ed97a1290370034232
Jonas Schwabe <jonas.schwabe@gmail.com>
2013-10-28 18:43:21 +0100

    Add Github Badge

--- tpl/default/head.php
+++ tpl/default/head.php
@@ -13,6 +13,7 @@
 	rel="stylesheet">
 </head>
 <body>
+<a href="https://github.com/gh1234/php-miniupload"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png" alt="Fork me on GitHub"></a>
 	<div class="navbar">
 		<div class="navbar-inner">
 			<a class="brand" href="http://ibait13.de">IBAIT 13</a>
